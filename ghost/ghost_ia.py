46f40a840b81af32420ef86d37eac7514c82fe94
ohabille <ohabille@aposte.net>
2021-06-07 07:12:12 +0200

    Reset

--- ghost/ghost_ia.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-""" Gère l'intelligence du ghost """
-
-class GhostIntelligency:
-    def __init__(self):
-        
--- main.py
+++ main.py
@@ -3,24 +3,4 @@
 
 # Import du script de gestion des paths des libraries
 # à commenter pour la compilation d'un executable
-import libs_paths
-
-# Library de gestion du ghost
-from ghost import Ghost
-# Library de gestion du GUI
-from gui import Gui
-# Library de gestion des events clavier
-from bind_key import BindK
-
-# Initialisation du GUI
-gui = Gui("Ghost")
-# Initialisation du ghost
-ghost = Ghost(gui)
-
-# Initialisation des event clavier
-BindK(gui, ghost)
-
-# Initialisation de hello du ghost
-
-# Mise en boucle du GUI
-gui.mainloop()
+# import libs_paths
